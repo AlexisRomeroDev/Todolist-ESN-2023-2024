1ac4660cccc7a6bfcf7182e15f65fdcbfba851a5
Alexis Romero <romero.alexis@free.fr>
2023-05-30 17:02:50 +0200

    Assets et Crud Todo

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Todo;
+use Faker\Factory;
+use Faker\Generator;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        for ($i=1; $i <= 50; $i++) {
+            $article = new Todo();
+            $article->setName($this->faker->sentence(4))
+                ->setDescription($this->faker->paragraph);
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
